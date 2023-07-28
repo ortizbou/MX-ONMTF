7c162c3b128e69fe24073e3b60f4b2a14a11a4ec
ortizbou <92049169+ortizbou@users.noreply.github.com>
2023-07-28 15:17:24 -0400

    patchmult.m

--- patchmult.m
+++ patchmult.m
@@ -1,17 +1,23 @@
-function [pos,q]=patchmult(a,H1,k1)
+function [pos,q]=patchmult(a,H,kc)
+
+% This function is used in the main algorithm of MX-ONMTF to post-process H and describe how the membership of the common
+% communities is determined across layers, i.e., which columns of H contain information about
+% the common communities present in layer l. This function is explained in Algorithm 3 in [1]
+% Reference: 
+% [1] “Community detection in multiplex networks based on orthogonal nonnegative matrix tri-factorization” Authors: Meiby Ortiz-Bouza and Selin Aviyente
 
 [~,n]=size(a);
-[M1,I1] = max(H1,[],2);
+[M1,I1] = max(H,[],2);
 
-h=zeros(size(H1));
+h=zeros(size(H));
 
-for i=1:k1
+for i=1:kc
 h(find(I1==i),i)=1;
 end
 
 
 %%% selecting the column that is not present in layer l
-for k=1:k1
+for k=1:kc
 [edges,~]=size(find(h(:,k)==1));
 di(1)=sum(sum(a.*(h(:,k)*h(:,k)')))/(edges*(edges-1));
 [rows,cols]=find((h(:,k)*h(:,k)')==1);
@@ -29,7 +35,6 @@ end
 
 
 q=di./d0;
-
 
 [~,pos]=sort(q);
 
